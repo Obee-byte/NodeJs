4d9d3688ed5a468313f688d9ef9b69b45266b727
Ilya <isultanov780@gmail.com>
2023-07-29 18:36:27 +0500

    Added CORS so we allow to POST from any kind device

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const productRoutes = require('./api/routes/products')
 
 const orderRoutes = require('./api/routes/orders')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS') {
+        res.header('Acess-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE')
+        return res.status(200).json({})
+    }
+})
 
 app.use('/products', productRoutes) // We actually using other file
 //insted of writing code here!
